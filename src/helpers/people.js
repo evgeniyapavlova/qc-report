17340b935a0019dae21d9822e7b867e2adf2c1e0
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2020-12-11 09:14:14 +0300

    add people

---
+++ src/helpers/people.js
@@ -0,0 +1,12 @@
+export default {
+  golubev: {
+    name: 'Vladimir Golubev',
+    position: 'Chief of Strategic Finance and Legal',
+    photo: '/assets/avatars/golubev.jpg',
+  },
+  bolotov: {
+    name: 'Kirill Bolotov',
+    position: 'Chief of B2B',
+    photo: '/assets/avatars/bolotov.jpg',
+  },
+};
