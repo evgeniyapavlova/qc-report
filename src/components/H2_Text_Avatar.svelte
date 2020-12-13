d2d92469912d08c2bec32cdccf7da6eee4076197
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2020-12-13 09:42:18 +0300

    add common component text + avatar

---
+++ src/components/H2_Text_Avatar.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Avatar from './Avatar.svelte';
+  export let person, h2, text;
+</script>
+
+<div class="wrap">
+  <h2>
+    {@html h2}
+  </h2>
+  <div class="flex">
+    <div class="wrap-avatar">
+      <Avatar {...person} />
+    </div>
+    <h4>{text}</h4>
+  </div>
+</div>
+
+<style>
+  h4 {
+    margin-top: 40px;
+    max-width: 710px;
+  }
+  h2 {
+    max-width: 530px;
+  }
+  @media only screen and (min-width: 800px) {
+    .flex {
+      display: flex;
+      flex-direction: row-reverse;
+      align-items: flex-start;
+    }
+    h4 {
+      margin-top: 0;
+      padding-right: 40px;
+    }
+  }
+  @media only screen and (min-width: 960px) {
+    .wrap-avatar {
+      position: absolute;
+      top: 0;
+			right: 0;
+    }
+    h4 {
+			width: calc(100% - 350px);
+    }
+    .wrap {
+      position: relative;
+    }
+    .flex {
+      display: block;
+    }
+  }
+</style>
