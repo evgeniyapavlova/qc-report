3520c2178bfc6eae22bba71d2689e6b6ab4f8f69
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2020-12-19 15:26:54 +0300

    add reguions dev

---
+++ src/components/3_MauPage/content/regions.js
@@ -0,0 +1,127 @@
+import people from '../../../helpers/people';
+const { lopez, rentoumis, kokh, nescheret, kulik } = people;
+export default [
+  {
+    person: lopez,
+    img: 'latam',
+    numbers: [
+      { period: '+126% <span>2019</span>', number: '+305%', caption: 'FTD' },
+      {
+        period: '+160,27% <span>2019</span>',
+        number: '+548% ',
+        caption: 'MAU',
+      },
+      { period: '+114% <span>2019</span>', number: '+324%', caption: 'PNL' },
+    ],
+    ru: {
+      title: 'Латинская Америка',
+      text:
+        'В этом году Латинская Америка выросла по всем ключевым метрикам. <br><br>Бразилия стала главным источником прибыли компании. Отсутствие конкурентов, сотрудничество с PayRetailers, работа маркетинга и несколько месяцев карантина способствовали росту нашей платформы в стране. <br><br>Колумбия, Мексика и Перу тоже вошли в список топ-стран в регионе. Это произошло благодаря активной работе наших партнерских программ, большое количество доступных клиентам платежных методов и характеристикам нашей платформы.',
+      quote: 'Бразилия стала главным источником прибыли компании',
+    },
+    en: {
+      title: 'Latin America',
+      text:
+        'This year, various reasons and events have made the LATAM region explode in terms of numbers. It has been the year in which Brazil has become the main source of the company. The scarcity of competitors plus the alliance with PayRetailers, added to our marketing and all this combined with the Lockdown months, have led to the total expansion of our platform. <br><br>Colombia, Mexico and Peru have become TOP countries thanks to penetration through affiliates, the wide range of payment methods and the speed and characteristics of our platform.The division of many departments by regions has allowed work in LATAM to be much more precise and effective. It’s one of the keys of success. In a year marked by long quarantines in almost all the countries in the region, companies like ours have exploded in the region.',
+      quote: 'Brazil has become the main source of the company',
+    },
+  },
+  {
+    reverse: true,
+    img: 'europe',
+    person: rentoumis,
+    numbers: [
+      { number: '+83,1% ', caption: 'FTD' },
+      { number: '–16,2%', caption: 'MAU' },
+      { number: '+10,3% ', caption: 'PNL' },
+    ],
+    ru: {
+      title: 'Европа',
+      text:
+        'Несмотря на то, что мы не успели выпустить наш новый торговый движок для европейских клиентов, мы превзошли результаты прошлого года и достигли всех целей 2020 года, кроме MAU. MAU оставался на низком уровне, потому что мы не вкладывали много средств в привлечение новых клиентов без нашего нового движка. <br><br>К счастью, как видно из цифр, прибыль в Европе составляла около 50%, что говорит о том, что в регионе есть много возможностей для активного роста в 2021 году.',
+      title2: 'Австралия',
+      text2:
+        'В Австралии в 2020 году мы работали над подготовкой новой структуры. Мы собрали команду опытных профессионалов, арендовали офис и подали заявку на получение лицензии ASIC. Надеемся, что в 2020 году мы получим лицензию и сможем незамедлительно начать работу.',
+      quote:
+        'В регионе есть много возможностей для активного роста  в 2021 году',
+    },
+    en: {
+      title: 'Europe',
+      text:
+        'Europe is a region with its own unique characteristics which make it different than the rest of the regions. Even though we didn’t manage to release our new trading engine for European clients in time we exceeded 2019 and reached all of our 2020 targets except MAU. MAU stayed at low levels because we haven’t invested a lot into bringing new clients without our new engine. <br><br>Fortunately, as it is evident from numbers, even by operating in a dormant way, Europe’s profit margins were at circa 50% which shows that there is a lot of room of growing dramatically in the region and that’s what we plan to do in 2021. Despite the small room for actions with small steps we achieved a significant improvement compared to 2019’s stats.',
+      quote: 'There is a lot of room of growing dramatically in the region',
+      title2: 'Australia',
+      text2:
+        'Regarding Australia, 2020 was a year of preparations of a new entity. We have assembled a team of experienced professionals, established office space and applied to ASIC for the necessary license. Hopefully, in 2020 we will get the green light from the authorities and we will able to start operating immediately.',
+    },
+  },
+  {
+    person: kokh,
+    img: 'asia',
+    numbers: [
+      {
+        period: '+120,57% <span>2019</span>',
+        number: '+48,3% ',
+        caption: 'FTD',
+      },
+      { period: '+53,94% <span>2019</span>', number: '+40,2%', caption: 'MAU' },
+      { period: '+53,39% <span>2019</span>', number: '+12,8%', caption: 'PNL' },
+    ],
+    ru: {
+      title: 'Азия',
+      text:
+        '2020 год был богат на экономические события, и это не могло не сказаться на прибыли, которую мы получили в регионе АСО. Мы смогли достичь отличных результатов и даже перевыполнить цели по большинству ключевых метрик, запланированных на этот год. <br><br>Хотелось бы поблагодарить всех, кто стоит за этими результатами. Маркетинг за то, что подхватывали волны и принимали верные решения по закупке. Технических специалистов за отличный продукт и стабильность платформы. Клиентский сервис за скорость и качество. Диллинг, биллинг, финансы и многие другие команды за то, что обеспечивали нас всем необходимым для работы. Вперед в 2021!',
+      quote: 'Мы смогли достичь отличных результатов и даже перевыполнить цели',
+    },
+    en: {
+      title: 'Asia',
+      text:
+        "The year 2020 was rich in economic events, and this could not but affect the profit that we received in the ACO region. We were able to achieve excellent results and even exceeded our goals for most of the key metrics planned for this year. <br><br>I'd like to thank everyone behind these results. Marketing for catching the waves and making the right purchasing decisions. Technical specialists for the excellent product and stability of the platform. Customer service for speed and quality. Dealing, Billing, Finance and many other teams for providing us with everything we needed to work. Onward to 2021!",
+      quote:
+        'We were able to achieve excellent results and even exceeded our goals',
+    },
+  },
+  {
+    reverse: true,
+    img: 'china',
+    person: nescheret,
+    numbers: [
+      { period: '+19,5% <span>2019</span>', number: '–50,1%', caption: 'FTD' },
+      { period: '+48,9% <span>2019</span>', number: '–31,86%', caption: 'MAU' },
+      { period: '+20% <span>2019</span>', number: '+62,78%', caption: 'PNL' },
+    ],
+    ru: {
+      title: 'Китай',
+      text:
+        'Китай остается очень сложным регионом для развития. Каждое изменение требует больших усилий. В этом году нам с трудом удалось получить разрешение на публикацию демо-приложения, которое мы в дальнейшем будем продвигать в сторах. После внесения всех необходимых изменений в приложение, наконец, можно будет сосредоточиться на мобильном направлении и привлечении новых пользователей. Остаются проблемы со стабильностью популярных китайских платежных систем. Мы также будем работать над их устранением и добавлением новых способов оплаты. <br><br>По сравнению с прошлым годом Китай стал более стабильным с точки зрения PNL. У нас есть стабильное ядро пользователей, которым нравится торговать на нашей платформе. Пока наше покрытие небольшое, и мы видим в этом возможность для развития.',
+      quote: 'Китай остается очень сложным регионом для развития',
+    },
+    en: {
+      title: 'China',
+      text:
+        'China remains a very difficult region to develop. Each movement requires a lot of effort and action. This year, with difficulty, we managed to obtain a patent for the Demo application, which we will promote in stores. After making all the required changes in the app, it will finally be possible to focus on the mobile direction and attract new users. There are still problems with the stability of popular Chinese payment systems. We will also work on fixing them and adding new payment methods. <br><br>Compared to last year, China has become more stable in terms of PNL and other things. There are no more months when there were big drops in PNL. We have a stable core of users who enjoy trading on our platform. But our coverage is still small and there is room to grow.',
+      quote: 'China remains a very difficult region to develop',
+    },
+  },
+  {
+    person: kulik,
+    img: 'africa',
+    numbers: [
+      { period: '+51,54% <span>2019</span>', number: '+75%', caption: 'FTD' },
+      { period: '+20,88% <span>2019</span>', number: '+55%', caption: 'MAU' },
+      { period: '+19,3% <span>2019</span>', number: '+20%', caption: 'PNL' },
+    ],
+    ru: {
+      title: 'Африка',
+      text:
+        '2020 год в Африке, как и во всем мире, прошел под знаком COVID19. Именно эпидемия и ее последствия предопределили наш результат. С учетом специфики региона, 2020 год можно считать успешным.<br><br> В годовом исчислении PNL вырос на 20%, а MAU на 55%. Основной рост регион показал в первом и втором кварталах. Март оказался для нас рекордным месяцем.<br><br> Из основных успехов хотелось бы выделить: прирост пользовательской базы почти на 100 тысяч пользователей и подключение Dusupay — платежного гейта, который позволяет нам принимать платежи через огромное количество платежных систем. К сожалению выполнить поставленные перед регионом цели по PNL нам не удалось.',
+      quote: 'Прирост пользовательской базы почти на 100K пользователей',
+    },
+    en: {
+      title: 'Africa',
+      text:
+        "The year 2020 in Africa, as in the rest of the world, was marked by COVID19. It was the epidemic and its aftermath that predetermined our results. Considering the specifics of the region, 2020 can be considered a success.<br><br> On a year-over-year basis, PNL is up 20% and MAU is up 55%. The region showed the main growth in the first and second quarters. March was a record month for us. <br><br>Among the main successes I'd like to highlight: the growth of user base of almost 100,000 users and the connection of Dusupay, a payment gateway that allows us to accept payments through a huge number of payment systems. Unfortunately, we were not able to meet the PNL goals set for the region.",
+      quote: 'The growth of user base of almost 100,000 users',
+    },
+  },
+];
---
+++ src/components/3_MauPage/sections/Regions.svelte
@@ -0,0 +1,51 @@
+<script>
+  import items from '../content/regions';
+
+  import RegionSection from '../components/RegionSection.svelte';
+
+  export let lang;
+  import Layout from '../../Layout.svelte';
+  import H2_Text_Avatar from '../../H2_Text_Avatar.svelte';
+  const content = {
+    ru: {
+      title: 'Развитие в регионах',
+      desc:
+        'Диверсификация прибыли по региону стала одной из наших стратегических целей на год. Над проектом работали региональные менеджеры Джоан Лопес, Степан Кулик, Борис Нещерет, Александр Кох и Никос Рентумис. Для выполнения целей по PnL и активным клиентам в регионах в этом году мы занимались подключением новых платежных методов, получением лицензий, развитием IB-системы, Customer Journey Map и многими другими инициативами.',
+    },
+    en: { title: '', desc: '' },
+  };
+</script>
+
+<section>
+  <Layout>
+    <div class="h2-wrap">
+      <H2_Text_Avatar
+        h2="{content[lang].title}"
+        person=""
+        text="{content[lang].desc}"
+      />
+    </div>
+    {#each items as item}
+      <RegionSection
+        person="{item.person}"
+        numbers="{item.numbers}"
+        title="{item[lang].title}"
+        title2="{item[lang].title2}"
+        text="{item[lang].text}"
+        text2="{item[lang].text2}"
+        quote="{item[lang].quote}"
+        reverse="{item.reverse}"
+        img="{item.img}"
+      />
+    {/each}
+  </Layout>
+</section>
+
+<style>
+  section {
+    overflow: hidden;
+  }
+  .h2-wrap {
+		margin-bottom: 100px;
+  }
+</style>
