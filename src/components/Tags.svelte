a3748b3f2aa5f7165ea7a9b6bab4df9cd46432ad
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2020-12-19 13:28:11 +0300

    add people

--- src/components/Tags.svelte
+++ src/components/Tags.svelte
@@ -2,6 +2,7 @@
   export let tags = [];
 </script>
 
+<div class="caption">Huge thanks to</div>
 <div class="tags">
   {#each tags as tag}
     <div class="tag">{tag}</div>
@@ -9,14 +10,21 @@
 </div>
 
 <style>
+  .caption {
+    color: var(--red);
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 22px;
+    margin-bottom: 40px;
+  }
   .tag {
     background: rgba(0, 0, 0, 0.1);
     border-radius: 5px;
     color: rgba(50, 62, 72, 0.6);
-		text-align: center;
-		padding: 7px 14px 6px;
-		display: inline-block;
-		margin-right: 20px;
-		margin-bottom: 10px;
+    text-align: center;
+    padding: 7px 14px 6px;
+    display: inline-block;
+    margin-right: 20px;
+    margin-bottom: 10px;
   }
 </style>
--- src/helpers/people.js
+++ src/helpers/people.js
@@ -14,4 +14,39 @@ export default {
     position: 'Chief of Global Compliance  and Back Office',
     photo: '/assets/avatars/francois.jpg',
   },
+  tychenko:{
+    name:'Evgeny Tychenko',
+    position: 'Chief of B2C',
+    photo: '/assets/avatars/tychenko.jpg',
+  },
+  pachko: {
+    name:'Artem Pachko',
+    position: 'Chief of Acquisition  and Sales',
+    photo: '/assets/avatars/pachko.jpg',
+  },
+  lopez: {
+    name:'Joan Lopez',
+    position: 'Regional Business Developer',
+    photo: '/assets/avatars/lopez.jpg',
+  },
+  rentoumis: {
+    name:'Nikos Rentoumis',
+    position: 'Regional Business Developer',
+    photo: '/assets/avatars/rentoumis.jpg',
+  },
+  kokh: {
+    name:'Alexander Kokh',
+    position: 'Regional Business Developer',
+    photo: '/assets/avatars/kokh.jpg',
+  },
+  nescheret: {
+    name:'Boris Nescheret',
+    position: 'Regional Business Developer',
+    photo: '/assets/avatars/nescheret.jpg',
+  },
+  kulik: {
+    name:'Stepan Kulik',
+    position: 'Regional Business Developer',
+    photo: '/assets/avatars/kulik.jpg',
+  }
 };
