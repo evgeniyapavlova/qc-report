a0372b5479e596269c7af574b9f07b683c593b7b
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2020-12-20 09:47:43 +0300

    add gh pages

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish(
+    '__sapper__/export',
+    {
+        branch: 'master',
+        repo: 'https://github.com/evgeniyapavlova/qc-report.git',
+        user: {
+            name: 'evgeniyapavlova',
+            email: 'evgeniya.pavlova.me@gmail.com'
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+);
