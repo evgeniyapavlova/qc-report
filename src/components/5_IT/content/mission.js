e3047b4289a6ace3dba14ac697980c12de972f36
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2020-12-29 00:54:39 +0300

    add mission

---
+++ src/components/5_IT/content/mission.js
@@ -0,0 +1,119 @@
+const content = {
+  ru: {
+    h5: 'От миссии компании к задачам разработки',
+    items: [
+      {
+        title: 'Mission Action Plan',
+        text: 'Общая картина того, что мы хотим достичь',
+        refresh: 'Ежегодно',
+        responsible: 'CEO',
+        caption:
+          'Показать миссию компании и то, какие большие шаги мы должны сделать для достижения этой миссии.',
+      },
+      {
+        title: 'Objectives tree',
+        text: 'Цели и ключевые результаты',
+        refresh: 'Раз в полгода',
+        responsible: 'CEO',
+        caption:
+          'Показать глобальный стратегический план компании и определить ключевые результаты для достижения поставленных целей.',
+      },
+      {
+        title: 'Roadmaps',
+        text:
+          'Показать долгосрочный план разработки и улучшения продукта. Согласовать продуктовые планы с целями компании.',
+        refresh: 'Раз в квартал',
+        responsible: 'Product Owner',
+        caption:
+          'Показать стратегический план развития и улучшения продукта. Согласовать продуктовые планы с целями компании',
+      },
+      {
+        title: 'Конкрети&shy;зировать фичи<br />(1-й этап плани&shy;рования)',
+        text:
+          'Получить фичи в разработку на следующий квартал с критериями их выполнения',
+        refresh: 'Раз в квартал',
+        responsible: 'CPO',
+        caption:
+          'Предоставить подробную и непротиворечивую информацию о фичах, которые будут разрабатываться. Координация требований со всеми заинтересованными сторонами. Определение блокеров.',
+      },
+      {
+        title: 'Оценить фичи<br />(2-й этап плани&shy;рования)',
+        text: 'Получить хорошо описанные фичи для анализа и оценки',
+        refresh: 'Раз в квартал',
+        responsible: 'CPO',
+        caption: 'Приоритезировать фичи на следующий квартал.',
+      },
+      {
+        title:
+          'Заплани&shy;ровать разработку фичей<br />(3-5-й этапы плани&shy;рования) ',
+        text:
+          'Планирование нагрузки разработки по фичам с учетом выставленных приоритетов.',
+        refresh: 'Раз в квартал',
+        responsible: 'Feature Owner',
+        caption:
+          'Запланировать распределение ресурсов разработки в соответствии с приоритетом фичей. Определить, какие фичи, скорее всего, будут реализованы, а какие точно не будут включены в квартальные планы и не будут сделаны. Определить риски разработки и согласовать план по управлению рисками.',
+      },
+    ],
+  },
+  en: {
+    items: [
+      {
+        title: 'Mission Action Plan',
+        text: 'Big picture what we should achieve',
+        refresh: 'Annualy',
+        responsible: 'CEO',
+        caption:
+          'To show propose and mission of the company + what big steps we should take in order to achieve this mission.',
+      },
+      {
+        title: 'Objectives tree',
+        text: 'Goals and KRs',
+        refresh: 'Once in half year',
+        responsible: 'CEO',
+        caption:
+          'To show the global strategic plan of the company and to put proper KRs to achieve this goals.',
+      },
+      {
+        title: 'Roadmaps',
+        text:
+          'To show long term plan of product development and evolution. To align product plans with company goals.',
+        refresh: 'Quarterly',
+        responsible: 'Product Owner',
+        caption: '',
+      },
+      {
+        title: 'To elaborate features<br /> (the 1st stage of Dev planning)',
+        text:
+          'To get features for development for the next quarter with criterias for their implementation',
+        refresh: 'Quarterly',
+        responsible: 'CPO',
+        caption:
+          'To provide detailed and consistent information on features going to development. Coordination of requirements with all stakeholders. Definition of implementation blockers.',
+      },
+      {
+        title: 'To score features<br /> (the 2nd stage of Dev planning)',
+        text:
+          'To get well-elaborated features for analysis and scoring (and a list that could not be worked out to cut off)',
+        refresh: 'Quarterly',
+        responsible: 'CPO',
+        caption: 'To prioritize features for the next quarter.',
+      },
+      {
+        title: 'To plan Dev of features<br /> (Stages 3–5 of Dev planning)',
+        text:
+          'Scheduling Dev resources occupation by features according to the given priorities.',
+        refresh: 'Quarterly',
+        responsible: 'Feature Owner',
+        caption:
+          'To plan development resources according to feature priorities. Indicate what features are likely to be made (commit), and what exactly will not be included in the quarterly plan and will not be done. Identify development risks and submit a risk management plan.',
+      },
+    ],
+  },
+};
+
+export default content;
+
+export const captions = {
+  ru: { cap1: 'Обновляется', cap2: 'Ответственный' },
+  en: { cap1: 'Refresh', cap2: 'Responsible' },
+};
---
+++ src/components/5_IT/sections/Mission.svelte
@@ -0,0 +1,235 @@
+<script>
+  export let lang;
+  import Layout from '../../Layout.svelte';
+  import content, { captions } from '../content/mission';
+  import arrowBottom from '../img/arrow_bottom.svg';
+</script>
+
+<Layout>
+  <h5 class="red">{content[lang].h5}</h5>
+  <div class="flex">
+    {#each content[lang].items as item, index}
+      <div class="item item-{index}">
+        <div class="title">
+          <div class="title-text">
+            {@html item.title}
+          </div>
+          {#if index !== 5}
+            <div class="svg-wrap svg-wrap-mission">
+              <svelte:component this="{arrowBottom}" />
+            </div>
+          {/if}
+        </div>
+        <div class="content">
+          <div class="text">{item.text}</div>
+          <img
+            src="{`ASSET_PREFIX/assets/it/img${index + 1}.jpg`}"
+            alt="Amaiz"
+          />
+          <div class="refresh small-text">
+            <span>{captions[lang].cap1}</span>{item.refresh}
+          </div>
+          <div class="responsible small-text">
+            <span>{captions[lang].cap2}</span>{item.responsible}
+          </div>
+          <div class="caption">{item.caption}</div>
+        </div>
+      </div>
+    {/each}
+  </div>
+</Layout>
+
+<style>
+  h5 {
+    margin-bottom: 160px;
+  }
+  .title-text {
+    margin-bottom: 70px;
+  }
+  img {
+    width: 100%;
+    max-width: 345px;
+    transform: translateY(-50%);
+  }
+  .title {
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 19px;
+    width: 100px;
+    min-width: 100px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .text {
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 17px;
+    color: rgba(50, 62, 72, 0.5);
+    display: none;
+  }
+  .small-text {
+    font-size: 13px;
+    line-height: 16px;
+  }
+  span {
+    color: rgba(50, 62, 72, 0.5);
+    display: block;
+  }
+  .caption {
+    font-size: 18px;
+    line-height: 22px;
+    margin-bottom: 70px;
+  }
+  .item {
+    display: flex;
+    margin-bottom: 80px;
+  }
+  .refresh {
+    margin-bottom: 13px;
+    margin-top: -40px;
+  }
+  .responsible {
+    margin-bottom: 35px;
+  }
+  .svg-wrap {
+    flex-grow: 1;
+    position: relative;
+    display: flex;
+    align-items: flex-end;
+  }
+  .svg-wrap:after {
+    content: '';
+    display: block;
+    width: 2px;
+    height: 100%;
+    background-color: var(--red);
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  @media only screen and (min-width: 480px) {
+    .title {
+      width: 150px;
+      min-width: 150px;
+    }
+    img {
+      transform: none;
+      margin-top: -130px;
+    }
+    .refresh {
+      margin-top: 0;
+    }
+    .caption {
+      margin-bottom: 100px;
+    }
+  }
+  @media only screen and (min-width: 960px) {
+    h5 {
+      margin-bottom: 70px;
+    }
+    .title {
+      font-size: 14px;
+      line-height: 17px;
+    }
+    .caption {
+      font-size: 16px;
+      line-height: 19px;
+    }
+    .text {
+      display: block;
+    }
+  }
+  @media only screen and (min-width: 960px) {
+    .flex {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
+    .item {
+      display: block;
+      width: calc(33% - 20px);
+    }
+    img {
+      margin-top: 0;
+    }
+    .title-text {
+      margin-bottom: 9px;
+    }
+    .text {
+      margin-bottom: 17px;
+    }
+    .title {
+      text-align: left;
+      flex-direction: row;
+      width: 100%;
+      min-height: 34px;
+    }
+    .text {
+      min-height: 51px;
+    }
+    .caption {
+      margin-bottom: 0;
+      padding-bottom: 20px;
+    }
+    .svg-wrap:after {
+      content: none;
+    }
+    :global(.svg-wrap-mission svg path) {
+      fill: transparent;
+    }
+    .refresh {
+      margin-top: 13px;
+    }
+    .svg-wrap {
+      background: url(ASSET_PREFIX/assets/svg/arrow-h.svg) no-repeat right
+        center;
+      height: 12px;
+      top: -3px;
+      margin-left: 15px;
+    }
+    .item-2 {
+      position: relative;
+    }
+    .item-2 .svg-wrap:after {
+      content: '';
+      display: block;
+      height: 2px;
+      width: 180px;
+      top: 4px;
+      right: 0;
+      left: auto;
+      transform: none;
+    }
+    .item-2 .svg-wrap {
+      background: none;
+    }
+    .item-2:before,
+    .item-2:after {
+      content: '';
+      display: block;
+      position: absolute;
+    }
+    .item-2:before {
+      bottom: -66px;
+      width: calc(100% * 3);
+      border-top: 2px solid var(--red);
+      height: 50px;
+      right: -20px;
+      border-left: 2px solid var(--red);
+    }
+    .item-2:after {
+      right: -20px;
+      width: 20px;
+      height: 100%;
+      border: 2px solid var(--red);
+      border-left: none;
+      top: 12px;
+    }
+    .item {
+      margin-bottom: 70px;
+    }
+  }
+</style>
