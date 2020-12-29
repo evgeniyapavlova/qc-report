530874dc7ff60b77a0c32797240c167e9d9b5630
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2020-12-29 20:54:12 +0300

    intro en

--- src/components/5_IT/content/intro.js
+++ src/components/5_IT/content/intro.js
@@ -6,7 +6,13 @@ const content = {
     p:
       'В&nbsp;этом году мы&nbsp;хорошо поработали над нашими ключевыми процессами поставки ценности, активно готовили&nbsp;IT к&nbsp;работе в&nbsp;условиях самых требовательных регуляторов, работали над инфраструктурой и&nbsp;сетью, улучшали качество данных, адаптировали платформу к&nbsp;B2B, оптимизировали транспортные узлы и&nbsp;многое-многое другое. Огромное спасибо вам ребята!',
   },
-  en: { h2: 'Raising the technological level of&nbsp;the company' },
+  en: {
+    h2: 'Raising the technological level of&nbsp;the company',
+    h4:
+      'We&nbsp;continue to&nbsp;actively develop cool products. And in&nbsp;order to&nbsp;do&nbsp;this in&nbsp;a&nbsp;professional and high-quality way, we&rsquo;re developing technology, infrastructure and processes.',
+    p:
+      'This year we&nbsp;worked well on&nbsp;our key value delivery processes, actively prepared&nbsp;IT for the most demanding regulators, worked on&nbsp;infrastructure and networking, improved data quality, adapted the platform to&nbsp;B2B, optimized transport hubs and much, much more. Thanks so&nbsp;much guys!',
+  },
 };
 
 export default content;
