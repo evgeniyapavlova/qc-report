1611ee09f9f57f047c49fff4f074eb354d5f00ba
evgeniyapavlova <evgeniya.pavlova.me@gmail.com>
2020-12-29 13:37:38 +0300

    Updates

---
+++ client/index.1c8859e1.js
@@ -0,0 +1,1 @@
+import{S as t,i as s,s as e,q as a,d as c,n}from"./client.bebdcf3e.js";function o(t){return{c(){this.h()},l(t){a('[data-svelte="svelte-mtyumh"]',document.head).forEach(c),this.h()},h(){document.title="ONE YEAR IN REVIEW"},m:n,p:n,i:n,o:n,d:n}}export default class extends t{constructor(t){super(),s(this,t,null,o,e,{})}}
