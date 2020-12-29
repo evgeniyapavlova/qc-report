1ff02fb4e5738dd0951713bf67e1f582a181b5b9
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2020-12-29 20:46:06 +0300

    cloud - EN

--- src/components/5_IT/content/cloud.js
+++ src/components/5_IT/content/cloud.js
@@ -21,7 +21,25 @@ const content = {
       },
     ],
   },
-  en: {},
+  en: {
+    h3: 'Using <span>cloud resources</span>',
+    p:
+      'There&rsquo;s been a&nbsp;tremendous amount of&nbsp;work done this year to&nbsp;tame Amazon. We&rsquo;re now able to&nbsp;use Amazon&rsquo;s services flexibly, at&nbsp;a&nbsp;much lower cost to&nbsp;us than to&nbsp;ordinary customers.',
+    quote:
+      'This is one of the fantastic developments in the architecture of the system',
+    items: [
+      {
+        title: 'Amazon Infrastructure',
+        text:
+          'We&nbsp;automated Amazon infrastructure: now it&rsquo;s easy to&nbsp;get a&nbsp;virtual server up&nbsp;and running and deploy K8S.',
+      },
+      {
+        title: 'Direct network connection',
+        text:
+          'A&nbsp;direct network connection between our main data center in&nbsp;Amsterdam and Amazon offers great opportunities:<br /><br />We&rsquo;ve tested a&nbsp;scheme in&nbsp;which we&nbsp;deploy special points of&nbsp;presence in&nbsp;Brazil on&nbsp;Amazon resources, which allows&nbsp;us to&nbsp;provide the best quality connection for our traders.',
+      },
+    ],
+  },
 };
 
 export default content;
