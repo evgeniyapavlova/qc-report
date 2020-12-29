181006cd94a1e64ae0d3a974df3f72074eef94c0
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2020-12-29 20:55:34 +0300

    portfolio en

--- src/components/5_IT/content/portfolio.js
+++ src/components/5_IT/content/portfolio.js
@@ -6,7 +6,13 @@ const content = {
     item_red: 'длились проектирование и разработка',
     quote: 'Это одно из феерических событий в архитектуре системы',
   },
-  en: {},
+  en: {
+    h3: '<span>Portfolio</span> launch',
+    p:
+      'This is&nbsp;one of&nbsp;the fantastic developments in&nbsp;the architecture of&nbsp;the system. We&nbsp;have finally launched Portfolio. The service is&nbsp;one of&nbsp;the busiest and calculates financial data for all platform clients in&nbsp;real time. For every tick of&nbsp;a&nbsp;market quote, the service must quickly calculate various financial indicators and pass them on&nbsp;to&nbsp;the client. This gives&nbsp;us a&nbsp;single calculation point and allows&nbsp;us the flexibility to&nbsp;change mechanisms without changing the client code. During the development process, a&nbsp;huge number of&nbsp;performance problems were solved, a&nbsp;huge number of&nbsp;debugging iterations were made, and we&nbsp;were successful.',
+    item_red: 'of design and development',
+    quote: 'This is one of the fantastic developments in the architecture of the system',
+  },
 };
 
 export default content;
