3291a493947eecef317ed26e811b23aabd4b4752
evgeniyapavlova <evgeniya.pavlova.me@gmail.com>
2020-12-30 14:08:32 +0300

    Updates

--- client/index.4a543554.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as s,s as e,q as a,d as c,n}from"./client.b3e3fc4d.js";function o(t){return{c(){this.h()},l(t){a('[data-svelte="svelte-mtyumh"]',document.head).forEach(c),this.h()},h(){document.title="ONE YEAR IN REVIEW"},m:n,p:n,i:n,o:n,d:n}}export default class extends t{constructor(t){super(),s(this,t,null,o,e,{})}}
